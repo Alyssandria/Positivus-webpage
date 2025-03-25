a890ebc583f15a00528af0c00651461831373bbf
Alyssandria <sanchez.roniel.work@gmail.com>
2025-03-25 04:47:34 -0400

    feat: teams section

--- src/pages/Home/sections/team/Team.tsx
+++ src/pages/Home/sections/team/Team.tsx
@@ -1,12 +1,15 @@
 import CONTENT from "@/lib/content/en_us.json"
 import { TeamCard } from "./components/TeamCards"
+import { Link } from "@/components/ui/Link"
 
 export const Teams = () => {
-  const TITLE = CONTENT.PUBLIC.MAIN.HOME.TEAM.TITLE
-  const SUBTITLE = CONTENT.PUBLIC.MAIN.HOME.TEAM.SUB_TITLE
+  const TITLE = CONTENT.PUBLIC.MAIN.HOME.TEAM.TITLE;
+  const SUBTITLE = CONTENT.PUBLIC.MAIN.HOME.TEAM.SUB_TITLE;
+  const TEAM_CTA = CONTENT.PUBLIC.MAIN.HOME.TEAM.CTA;
   const teamCards = CONTENT.PUBLIC.MAIN.HOME.TEAM.CARDS.map((data) => {
     return <TeamCard data={data} key={data.NAME} />
-  })
+  });
+
   return (
     <section className="p-4 space-y-6">
       <div className="space-y-6">
@@ -15,7 +18,12 @@ export const Teams = () => {
         </h3>
         <p className="text-center font-medium">{SUBTITLE}</p>
       </div>
-      {teamCards}
+
+      <div className="flex flex-col gap-6">
+        {teamCards}
+      </div>
+
+      <Link to={TEAM_CTA.ACTION} linkStyle="button_bg" className="text-center font-normal text-base inline-block w-full bg-black">{TEAM_CTA.CONTENT}</Link>
     </section>
   )
 }
--- src/pages/Home/sections/team/components/TeamCards.tsx
+++ src/pages/Home/sections/team/components/TeamCards.tsx
@@ -1,5 +1,6 @@
 import { TeamBg } from "@/components/icons/TeamBg";
 import { cn } from "@/lib/utils";
+import { Linkedin } from "lucide-react";
 import { HTMLAttributes } from "react";
 
 
@@ -14,8 +15,25 @@ interface TeamCardProps extends HTMLAttributes<HTMLDivElement> {
 export const TeamCard = ({ className, data, ...props }: TeamCardProps) => {
 
   return (
-    <div className={cn("w-full", className)} {...props}>
-      <TeamBg imgLink={data.PROFILE} />
+    <div className={cn("w-full service-card-shadow border rounded-4xl max-xs:p-card p-card-mob divide-y space-y-8 divide-black", className)} {...props}>
+      <div className="flex relative gap-4 w-full pb-6">
+        <figure className="w-50">
+          <TeamBg imgLink={data.PROFILE} />
+          <figcaption className="sr-only">{`An avatar of ${data.NAME}`}</figcaption>
+        </figure>
+        <div className="w-full flex flex-col gap-1 justify-end">
+          <span className="font-bold block max-xs:w-min text-lg ">{data.NAME}</span>
+          <span className="inline-block font-medium">{data.TITLE}</span>
+        </div>
+        <div className="absolute p-2 size-8 top-0 right-0 bg-secondary rounded-full">
+          <Linkedin className="size-full text-primary fill-primary" />
+        </div>
+      </div>
+      <div>
+        <p className="font-medium">{data.BIO}</p>
+      </div>
+
+
     </div>
   );
 };
