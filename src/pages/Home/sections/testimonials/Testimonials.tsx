e0fa7dc67cba12b28f949d803c33a034d09fe80d
Alyssandria <sanchez.roniel.work@gmail.com>
2025-05-13 05:04:45 -0400

    feat: carousel infinite wip

--- src/pages/Home/sections/testimonials/Testimonials.tsx
+++ src/pages/Home/sections/testimonials/Testimonials.tsx
@@ -3,7 +3,6 @@ import { ClientTestimony } from "./components/ClientTestimony"
 import { MoveLeft, MoveRight } from "lucide-react"
 import { CarouselIcon } from "@/components/icons/CarouselIcon"
 import { useRef, useState, useEffect } from "react"
-import { cn } from "@/lib/utils"
 import { Section } from "@/components/Section"
 
 export const Testimonials = () => {
@@ -15,11 +14,11 @@ export const Testimonials = () => {
   const clientCardsRefMap = useRef<Map<HTMLElement | null, number>>(new Map()) // TO STORE THE HTML ELEMENT FOR LOOKUPS
   const clientCards = CLIENTS.map((data, i) => {
     return (
-      <ClientTestimony key={data.NAME} data={data}
+      <ClientTestimony key={data.NAME} data={data} className="md:w-4/5"
         ref={
           (el) => {
             clientCardRefs.current[i] = el;
-            clientCardsRefMap.current.set(el, i);
+            clientCardsRefMap.current.set(el, i + 1); // + 1 OFFSET FOR THE CLONES
           }}
       />
     )
@@ -27,19 +26,26 @@ export const Testimonials = () => {
 
   // CAROUSEL LOGIC
   const STEP = 1 // AMOUNT TO DECREMENT OR INCREMENT BY
-  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [activeIndex, setActiveIndex] = useState<number>(1);
   const carouselContainer = useRef<HTMLDivElement | null>(null);
   const intersectionObserver = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    if (!carouselContainer.current) return;
+    console.log(carouselContainer.current.children)
+
+    carouselContainer.current.children[1].scrollIntoView({ behavior: "instant" })
+  }, []);
 
   useEffect(() => {
     if (!carouselContainer.current) return;
     intersectionObserver.current = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
+
           const index = clientCardsRefMap.current.get(entry.target as HTMLElement)
-          if (index !== undefined && activeIndex !== index) {
-            setActiveIndex(index)
-          }
+          console.log(index);
+          if (index !== undefined && activeIndex !== index) setActiveIndex(index)
         }
       },);
     }, { root: carouselContainer.current, threshold: .90 });
@@ -57,31 +63,33 @@ export const Testimonials = () => {
 
   const handleClickPrev = () => {
     if (!carouselContainer.current) return;
-    carouselContainer.current.children[activeIndex - STEP].firstElementChild?.scrollIntoView({ behavior: "smooth" })
+    carouselContainer.current.children[activeIndex - STEP].firstElementChild?.scrollIntoView({ behavior: "smooth", block: "nearest" })
   }
 
   const handleCLickNext = () => {
     if (!carouselContainer.current) return;
-    carouselContainer.current.children[activeIndex + STEP].firstElementChild?.scrollIntoView({ behavior: "smooth" })
+    carouselContainer.current.children[activeIndex + STEP].firstElementChild?.scrollIntoView({ behavior: "smooth", block: "nearest" })
   }
 
 
 
   return (
     <Section title={TITLE} subTitle={SUBTITLE}>
-      <div className="bg-secondary p-6 pb-8 space-y-6 rounded-[45px]">
+      <div className="bg-secondary p-6 pb-8 flex flex-col gap-6 items-center rounded-[45px]">
         <div ref={carouselContainer} className="w-full flex items-center rounded-[45px] cards-swipe overflow-x-scroll gap-10">
+          <ClientTestimony data={CLIENTS[CLIENTS.length - 1]} className="md:w-1/2" ref={(el) => { clientCardsRefMap.current.set(el, 7) }} />
           {clientCards}
+          <ClientTestimony data={CLIENTS[0]} className="md:w-1/2" ref={(el) => { clientCardsRefMap.current.set(el, 0) }} />
         </div>
 
-        <div className="flex justify-around items-center">
-          <button onClick={handleClickPrev} className={cn(activeIndex === 0 ? "carousel-disabled" : "")} disabled={activeIndex === 0}>
+        <div className="flex justify-around items-center md:w-1/2">
+          <button onClick={handleClickPrev}>
             <MoveLeft className="stroke-white size-8" />
           </button>
           <div className="flex gap-2">
             {carouselIcons}
           </div>
-          <button onClick={handleCLickNext} className={activeIndex === CLIENTS.length - 1 ? "carousel-disabled" : ""} disabled={activeIndex === CLIENTS.length - 1}>
+          <button onClick={handleCLickNext}>
             <MoveRight className="stroke-white size-8" />
           </button>
         </div>
